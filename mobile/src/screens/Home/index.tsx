bbf737a85514f5db293e36cf24b91bd29339f462
HenrickyL <Henrickyl1@gmail.com>
2022-09-13 01:14:22 -0300

    logo & heading on home

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,9 +1,13 @@
-import { View } from "react-native"
+import { View, Image } from "react-native"
 import {styles} from './styles'
+import logoImage from '../../assets/logo-nlw-esports.png'
+import { Heading } from "../../components/Heading"
+
 export const Home = ()=>{
     return (
         <View style={styles.container}> 
-
+            <Image source={logoImage} style={styles.logo}/>
+            <Heading title="Encontre seu duo" subtitle="Selecione o game que deseja jogar..."/>
         </View>
     )
 }
