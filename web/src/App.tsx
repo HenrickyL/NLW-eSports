f1b35d0212f27eb6ed31c7ee19cbd6ea022aaa52
HenrickyL <Henrickyl1@gmail.com>
2022-09-17 13:42:40 -0300

    create cards dynamic

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,19 @@
 import "./styles/main.css"
 // @ts-ignore
 import logoImg from './assets/logo-nlw-esports.svg'
-import GameCard from "./components/GameCard"
-import {MagnifyingGlassPlus as Search} from 'phosphor-react'
+import GameCard, { IGame } from "./components/GameCard"
+import { CreateAdBanner } from "./components/CreateAdBanner"
+import { useEffect, useState } from "react"
 
 function App() {
-
+  var [games, setGames] = useState<IGame[]>([])
+  useEffect(()=>{
+    fetch('http://localhost:3333/games')
+      .then(res=> res.json())
+      .then(data => {
+        setGames(data)
+      })
+  },[])
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt="" />
@@ -13,25 +21,11 @@ function App() {
         Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui</h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <GameCard name="League of Legends" imgUrl="/game-1.png" anuncios={4}/>
-        <GameCard name="Apex Legends" imgUrl="/game-4.png" anuncios={4}/>
-        <GameCard name="Counter Strike" imgUrl="/game-3.png" anuncios={4}/>
-        <GameCard name="World of Warcraft" imgUrl="/game-6.png" anuncios={4}/>
-        <GameCard name="Dota 2" imgUrl="/game-2.png" anuncios={4}/>
-        <GameCard name="Fortnite" imgUrl="/game-5.png" anuncios={4}/>
+        {games.map((game)=>(
+          <GameCard key={game.id} data={game}/>
+        ))}
       </div>
-      <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden">
-        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
-          <div>
-            <strong className="text-2xl text-white font-black block">Não encontrou seu duo?</strong>
-            <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
-          </div>
-          <button className="py-3 px-4 bg-violet-500 text-white rounded-md hover:bg-violet-600 
-          transition duration-500 flex items-center gap-3">
-            <Search size={24}/>
-            Publicar anúncio</button>
-        </div>
-      </div>
+      <CreateAdBanner />
 
     </div>
   )
--- web/src/components/GameCard/index.tsx
+++ web/src/components/GameCard/index.tsx
@@ -1,15 +1,20 @@
-interface GameCardProps {
-    name: string;
-    anuncios: number;
-    imgUrl: string
+export interface IGame {
+    id: string;
+    title: string;
+    _count: {
+      ads:number}
+    bannerUrl: string
 }
-const GameCard = (prop:GameCardProps)=>{
+interface Prop{
+  data: IGame
+}
+const GameCard = ({data}:Prop)=>{
     return (
         <a href="" className="relative rounded-lg overflow-hidden">
-          <img src={prop.imgUrl} alt="Game image" />
+          <img src={data.bannerUrl} alt="Game image" />
           <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0 left-0">
-            <strong className="font-bold text-white block">{prop.name}</strong>
-            <span className="text-zinc-300 text-sm block">{prop.anuncios} anúncios</span>
+            <strong className="font-bold text-white block">{data.title}</strong>
+            <span className="text-zinc-300 text-sm block">{data._count.ads} anúncio(s)</span>
           </div>
         </a>
     )
